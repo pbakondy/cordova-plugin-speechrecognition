888280dc71c177dcefabd2746e72223eac4f0fce
Peter Bakondy <pbakondy@gmail.com>
2016-10-03 12:59:14 +0200

    Cleanup

--- www/speechRecognition.js
+++ www/speechRecognition.js
@@ -5,7 +5,7 @@ module.exports = {
   startListening: function(successCallback, errorCallback, language, matches, prompt) {
     cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'startListening', [language, matches, prompt]);
   },
-  stopListening: function(successCallback, errorCallback, language, matches, prompt) {
+  stopListening: function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'stopListening', []);
   },
   getSupportedLanguages: function(successCallback, errorCallback) {
