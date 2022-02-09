85bdcf7e4fc95821bd8f85521e8d1452626d433e
zafirskthelifehacker <zafirskthelifehacker@hotmail.com>
2022-02-09 10:18:36 +0400

    Reset Line 84

--- src/ios/SpeechRecognition.m
+++ src/ios/SpeechRecognition.m
@@ -81,8 +81,8 @@
         }
 
         AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
-        [audioSession setMode:AVAudioSessionModeMeasurement error:nil];
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
+        [audioSession setMode:AVAudioSessionModeDefault error:nil];
         [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
 
         self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
